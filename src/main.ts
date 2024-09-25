4ddc645053107708d10b41fc683b03c80ddcbdb3
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2024-09-25 12:01:51 -0400

    Sección 8

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,15 @@ import { env } from 'process';
 
 async function bootstrap() {
   const logger = new Logger('Main');
+
+  console.log(envs.natsServers);
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.TCP,
+      transport: Transport.NATS,
       options: {
-        port: envs.port,
+        servers: envs.natsServers,
       },
     },
   );
